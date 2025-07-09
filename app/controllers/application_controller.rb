11547d62105202a80c5321a359600c717e123327
Aleksei Hrynevich <lechan662@gmail.com>
2025-07-09 14:24:12 +0200

    [BPL-4] Fixed issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   # adds some site-wide behavior into the application controller
   include CommonwealthVlrEngine::Controller
 
-  layout :determine_layout if respond_to? :layout
+  # layout :determine_layout if respond_to? :layout
 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
