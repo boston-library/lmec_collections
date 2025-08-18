24e5fd26e8193939271f7c73a77224050a3e1848
Eben English <eben.english@gmail.com>
2025-08-18 15:57:30 -0400

    custom collections search builder

---
+++ app/models/collections_search_builder.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# use local class so we can remove :institution_limit from default CommonwealthVlrEngine::CollectionsSearchBuilder
+class CollectionsSearchBuilder < Blacklight::SearchBuilder
+  include Blacklight::Solr::SearchBuilderBehavior
+  include CommonwealthVlrEngine::SearchBuilderBehavior
+
+  self.default_processor_chain += [
+    :site_filter, :exclude_unpublished_items, :collections_filter, :starts_with
+  ]
+end
