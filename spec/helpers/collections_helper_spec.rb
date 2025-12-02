bb8d6da29b21c86d60c5f186b86785cb4bc1d702
Eben English <ebenenglish@users.noreply.github.com>
2025-12-02 16:22:51 -0500

    use date asc as default sort for #link_to_all_col_items

---
+++ spec/helpers/collections_helper_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe CollectionsHelper do
+  let(:blacklight_config) { CatalogController.blacklight_config }
+  let(:document) do
+    { blacklight_config.index.title_field.field => 'Foo Collection',
+      blacklight_config.institution_field.to_sym => 'Bar Institution',
+      blacklight_config.hosting_status_field.to_sym => 'harvested' }
+  end
+
+  before(:each) do
+    without_partial_double_verification do
+      allow(helper).to receive_messages(blacklight_config: blacklight_config)
+    end
+  end
+
+  describe '#link_to_all_col_items' do
+    it 'creates a search link with the correct default sort' do
+      expect(helper.link_to_all_col_items(document)).to include("sort=date_start_dtsi+asc")
+    end
+  end
+end
