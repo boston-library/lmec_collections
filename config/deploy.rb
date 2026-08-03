12670b5d57feefe3d7292edb5e1dea9e69c12dc5
Eben English <ebenenglish@users.noreply.github.com>
2026-08-03 12:26:42 -0400

    update capistrano version in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # config valid for current version and patch releases of Capistrano
-lock '~> 3.19.2'
+lock '~> 3.20.1'
 
 set :use_sudo, false
 ## STAGE_NAME is a paramter from Jenkins job: "staging", "qc", and "testing"
